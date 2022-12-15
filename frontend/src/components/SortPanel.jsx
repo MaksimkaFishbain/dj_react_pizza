b71a6181662ec22d983db6797dbe2db29fb32a65
Spenkau <96494361+Spenkau@users.noreply.github.com>
2022-12-15 19:51:18 +0300

    fad:featured Scroll range added

--- frontend/src/components/SortPanel.jsx
+++ frontend/src/components/SortPanel.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import Scroll from "./UI/Scroll";
 
-const SortPanel = ({setIsRender, setFillerValue, sortByField, filterByFiller}) => {
+const SortPanel = ({setIsRender, sortByField, setFillerValue, filterByFiller}) => {
+    const [rangeValue, setRangeValue] = React.useState([10, 100]);
+
     return (
         <section className="sortPanel">
             <ol>
-                <li padding-right="300px" onClick={() => {
+                <li onClick={() => {
                     setIsRender(filterByFiller(1))
                     setFillerValue(1)
                 }}>Мясные</li>
-                <li padding-right="400px" onClick={() => {
+                <li onClick={() => {
                     setIsRender(filterByFiller(0))
                     setFillerValue(0)
                 }}>Вегетарианские</li>
-                <li padding-right="200px" onClick={() => {
+                <li onClick={() => {
                     setIsRender(filterByFiller(2))
                     setFillerValue(2)
                 }}>Все</li>
             </ol>
             <ol>
-                <Scroll />
+                <Scroll
+                    setIsRender={setIsRender}
+                    rangeValue={rangeValue}
+                    setRangeValue={i => setRangeValue(i)}
+                    sortByField={sortByField}
+                />
+                <div className={"reset-btn"} onClick={() => {
+                    setRangeValue([10,100]);
+                    setIsRender(sortByField())
+                }}>
+                    <h4>Сбросить настройки</h4>
+                </div>
             </ol>
             <ol>
                 <p>Сортировать по:</p>
--- frontend/src/components/UI/Scroll.jsx
+++ frontend/src/components/UI/Scroll.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import { Slider, Switch } from 'antd';
-const App = () => {
-    const [disabled, setDisabled] = useState(false);
-    const onChange = (checked) => {
-        setDisabled(checked);
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+export default function RangeSlider({setIsRender, sortByField, rangeValue, setRangeValue}) {
+
+
+    const handleChange = (event, newValue) => {
+        setRangeValue(newValue);
+        setIsRender(sortByField().filter((item) => {
+            return item.price < rangeValue[1] && item.price > rangeValue[0]
+        }))
     };
+
     return (
-        <>
-            <Slider defaultValue={30} disabled={disabled} />
-            <Slider range defaultValue={[20, 50]} disabled={disabled} />
-            Disabled: <Switch size="small" checked={disabled} onChange={onChange} />
-        </>
+        <Box sx={{ width: 450 }}>
+            <Slider
+                getAriaLabel={() => 'Temperature range'}
+                value={rangeValue}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                // getAriaValueText={'hello'}
+            />
+        </Box>
     );
-};
-export default App;
+}
